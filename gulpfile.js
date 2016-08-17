f382bd182ff94c6aaaa7bd998b22ec5a73affefa
Blackie Tsai <blackie1019@gmail.com>
2016-08-18 01:18:32 +0800

    Web optimized

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,102 @@
 var gulp = require('gulp');
-var cleanCSS = require('gulp-clean-css');
 var uglify = require('gulp-uglify');
-var htmlmin = require('gulp-htmlmin');
-var htmlclean = require('gulp-htmlclean');
+var cleanCSS = require('gulp-clean-css');
+var concat = require('gulp-concat');
+var runSequence = require('run-sequence');
+var del = require('del');
 
-// 压缩 public 目录 css
-gulp.task('minify-css', function() {
-    return gulp.src('./public/**/*.css')
-        .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(gulp.dest('./public'));
+var public_dir = __dirname + '/public';
+var bundle_dir = public_dir + '/bundle';
+var bundle_js = bundle_dir + '/js'
+var bundle_css = bundle_dir + '/css';
+var bundle_fonts = public_dir + '/fonts';
+
+gulp.task('clean', function() {
+    return del([bundle_dir + '/*']);
 });
-// 压缩 public 目录 html
-gulp.task('minify-html', function() {
-  return gulp.src('./public/**/*.html')
-    .pipe(htmlclean())
-    .pipe(htmlmin({
-         removeComments: true,
-         minifyJS: true,
-         minifyCSS: true,
-         minifyURLs: true,
-    }))
-    .pipe(gulp.dest('./public'))
-});
-// 压缩 public/js 目录 js
+
+gulp.task('copy-js', function() {
+    return gulp.src([
+             public_dir + '/vendors/jquery/index.js',
+             public_dir + '/vendors/fastclick/lib/fastclick.min.js',
+             public_dir + '/vendors/jquery_lazyload/jquery.lazyload.js',
+             public_dir + '/vendors/velocity/velocity.min.js',
+             public_dir + '/vendors/velocity/velocity.ui.min.js',
+             public_dir + '/vendors/fancybox/source/jquery.fancybox.pack.js',
+             public_dir + '/js/src/utils.js',
+             public_dir + '/js/src/motion.js',
+             public_dir + '/js/src/bootstrap.js',
+             public_dir + '/js/src/scrollspy.js',
+             public_dir + '/js/src/post-details.js'
+
+        ])
+        .pipe(gulp.dest(bundle_js));
+})
+
 gulp.task('minify-js', function() {
-    return gulp.src('./public/**/*.js')
+    return gulp.src(bundle_js)
         .pipe(uglify())
-        .pipe(gulp.dest('./public'));
+        .pipe(gulp.dest(bundle_js));
+})
+
+gulp.task('concat-js', function() {
+    return gulp.src([
+             bundle_js + '/index.js',
+             bundle_js + '/fastclick.min.js',
+             bundle_js + '/jquery.lazyload.js',
+             bundle_js + '/velocity.min.js',
+             bundle_js + '/velocity.ui.min.js',
+             bundle_js + '/jquery.fancybox.pack.js',
+             bundle_js + '/utils.js',
+             bundle_js + '/motion.js',
+             bundle_js + '/bootstrap.js',
+             bundle_js + '/scrollspy.js',
+             bundle_js + '/post-details.js',
+
+        ])
+        .pipe(concat('all.min.js'))
+        .pipe(gulp.dest(bundle_dir));
 });
-// 执行 gulp 命令时执行的任务
-gulp.task('default', [
-    'minify-html','minify-css','minify-js'
-]);
+
+gulp.task('copy-css', function() {
+    return gulp.src([
+            public_dir + '/vendors/fancybox/source/jquery.fancybox.css',
+            public_dir + '/vendors/font-awesome/css/font-awesome.min.css',
+            public_dir + '/css/main.css'
+        ])
+        .pipe(gulp.dest(bundle_css));
+})
+
+
+gulp.task('copy-font-awesome', function() {
+    return gulp.src(public_dir + '/vendors/font-awesome/fonts/**.*')
+        .pipe(gulp.dest(bundle_fonts));
+})
+
+gulp.task('minify-css', function() {
+    return gulp.src(bundle_css)
+        .pipe(cleanCSS({
+            debug: true
+        }, function(details) {
+            console.log(`${details.name}:[${Math.round(details.stats.efficiency *100)}%]${details.stats.originalSize}=>${details.stats.minifiedSize}`);
+        }))
+        .pipe(gulp.dest(bundle_css));
+})
+
+gulp.task('concat-css', function() {
+    return gulp.src([
+            bundle_css + '/jquery.fancybox.css',
+            bundle_css + '/font-awesome.min.css',
+            bundle_css + '/main.css'
+        ])
+        .pipe(concat('all.min.css'))
+        .pipe(gulp.dest(bundle_dir));
+});
+
+gulp.task('finish-task', function() {
+    return del([bundle_js, bundle_css]);
+});
+
+gulp.task('default', function() {
+    runSequence('clean', ['copy-js', 'copy-css','copy-font-awesome'], ['minify-js', 'minify-css'], ['concat-js', 'concat-css'],'finish-task');
+});
